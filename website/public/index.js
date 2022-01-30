928473fc1fab73e4dbe454efb81e8f786b092411
BensonYang <benson2219@gmail.com>
2022-01-30 17:03:46 +0800

    2022/1/30 test 2

--- website/public/index.js
+++ website/public/index.js
@@ -4,44 +4,56 @@ var xValues = []
 var data_count = 0;
 
 const ctx = document.getElementById('myChart').getContext('2d');
-var history = new Chart(ctx, {
+const history = new Chart(ctx, {
     type: "line",
     data: {
         labels: xValues,
         datasets: [{
+            label: 'PM 1.0 std',
             fill: false,
             borderColor: "rgba(255,0,0,1.0)",
         },{
+            label: 'PM 2.5 std',
             fill: false,
             borderColor: "rgba(255,140,0,1.0)",
         },{
+            label: 'PM 10 std',
             fill: false,
             borderColor: "rgba(255,255,0,1.0)",
         },{
+            label: 'PM 1.0 env',
             fill: false,
             borderColor: "rgba(140,255,0,1.0)",
         },{
+            label: 'PM 2.5 env',
             fill: false,
             borderColor: "rgba(0,255,0,1.0)",
         },{
+            label: 'PM 10 env',
             fill: false,
             borderColor: "rgba(0,255,140,1.0)",
         },{
+            label: 'Particles > 0.3um',
             fill: false,
             borderColor: "rgba(0,255,255,1.0)",
         },{
+            label: 'Particles > 0.5um',
             fill: false,
             borderColor: "rgba(0,0,255,1.0)",
         },{
+            label: 'Particles > 1.0um',
             fill: false,
             borderColor: "rgba(110,0,255,1.0)",
         },{
+            label: 'Particles > 2.5um',
             fill: false,
             borderColor: "rgba(170,0,255,1.0)",
         },{
+            label: 'Particles > 5.0um',
             fill: false,
             borderColor: "rgba(255,0,255,1.0)",
         },{
+            label: 'Particles > 10.0 um',
             fill: false,
             borderColor: "rgba(255,0,150,1.0)",
         }]
