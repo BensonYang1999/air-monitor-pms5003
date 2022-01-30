22516410880aabf11aa81c5dd46bac633999c4d7
BensonYang <benson2219@gmail.com>
2022-01-30 19:54:32 +0800

    2022/1/30 chart split

--- website/public/index.js
+++ website/public/index.js
@@ -3,8 +3,8 @@ const socket = io.connect(window.location.origin);
 var xValues = []
 var data_count = 0;
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const history = new Chart(ctx, {
+//const ctx = document.getElementById('Chart1').getContext('2d');
+const chart_1 = new Chart('Chart1', {
     type: "line",
     data: {
         labels: xValues,
@@ -32,7 +32,81 @@ const history = new Chart(ctx, {
             label: 'PM 10 env',
             fill: false,
             borderColor: "rgba(0,255,140,1.0)",
+        }/*, {
+            label: 'Particles > 0.3um (/100)',
+            fill: false,
+            borderColor: "rgba(0,255,255,1.0)",
         }, {
+            label: 'Particles > 0.5um (/100)',
+            fill: false,
+            borderColor: "rgba(0,0,255,1.0)",
+        }, {
+            label: 'Particles > 1.0um (/10)',
+            fill: false,
+            borderColor: "rgba(110,0,255,1.0)",
+        }, {
+            label: 'Particles > 2.5um',
+            fill: false,
+            borderColor: "rgba(170,0,255,1.0)",
+        }, {
+            label: 'Particles > 5.0um',
+            fill: false,
+            borderColor: "rgba(255,0,255,1.0)",
+        }, {
+            label: 'Particles > 10.0 um',
+            fill: false,
+            borderColor: "rgba(255,0,150,1.0)",
+        }*/]
+    },
+    options: {
+        legend: { display: false }/*,
+        scales: {
+            yAxes: [{ ticks: { min: 6, max: 16 } }],
+        },
+        plugins: {
+            zoom: {
+                zoom: {
+                    wheel: {
+                        enabled: true,
+                    },
+                    pinch: {
+                        enabled: true
+                    },
+                    mode: 'xy',
+                }
+            }
+        }*/
+    }
+});
+const chart_2 = new Chart('Chart2', {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [/*{
+            label: 'PM 1.0 std',
+            fill: false,
+            borderColor: "rgba(255,0,0,1.0)",
+        }, {
+            label: 'PM 2.5 std',
+            fill: false,
+            borderColor: "rgba(255,140,0,1.0)",
+        }, {
+            label: 'PM 10 std',
+            fill: false,
+            borderColor: "rgba(255,255,0,1.0)",
+        }, {
+            label: 'PM 1.0 env',
+            fill: false,
+            borderColor: "rgba(140,255,0,1.0)",
+        }, {
+            label: 'PM 2.5 env',
+            fill: false,
+            borderColor: "rgba(0,255,0,1.0)",
+        }, {
+            label: 'PM 10 env',
+            fill: false,
+            borderColor: "rgba(0,255,140,1.0)",
+        }, */{
             label: 'Particles > 0.3um (/100)',
             fill: false,
             borderColor: "rgba(0,255,255,1.0)",
@@ -81,7 +155,8 @@ const history = new Chart(ctx, {
 
 socket.on("new_data", data => {
     data_count += 1;
-    addData(history, data_count, data)
+    addData(chart_1, data_count, data.slice(0, 6));
+    addData(chart_2, data_count, data.slice(6, 12));
     //history.addData(data, ++data_count)
 });
 
