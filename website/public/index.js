88ebe754c7b84bd9e97b7fdfbc8d16d7740891f3
BensonYang <benson2219@gmail.com>
2022-01-30 19:28:36 +0800

    2022/1/30 add chart zoom

--- website/public/index.js
+++ website/public/index.js
@@ -12,57 +12,70 @@ const history = new Chart(ctx, {
             label: 'PM 1.0 std',
             fill: false,
             borderColor: "rgba(255,0,0,1.0)",
-        },{
+        }, {
             label: 'PM 2.5 std',
             fill: false,
             borderColor: "rgba(255,140,0,1.0)",
-        },{
+        }, {
             label: 'PM 10 std',
             fill: false,
             borderColor: "rgba(255,255,0,1.0)",
-        },{
+        }, {
             label: 'PM 1.0 env',
             fill: false,
             borderColor: "rgba(140,255,0,1.0)",
-        },{
+        }, {
             label: 'PM 2.5 env',
             fill: false,
             borderColor: "rgba(0,255,0,1.0)",
-        },{
+        }, {
             label: 'PM 10 env',
             fill: false,
             borderColor: "rgba(0,255,140,1.0)",
-        },{
+        }, {
             label: 'Particles > 0.3um (/100)',
             fill: false,
             borderColor: "rgba(0,255,255,1.0)",
-        },{
+        }, {
             label: 'Particles > 0.5um (/100)',
             fill: false,
             borderColor: "rgba(0,0,255,1.0)",
-        },{
+        }, {
             label: 'Particles > 1.0um (/10)',
             fill: false,
             borderColor: "rgba(110,0,255,1.0)",
-        },{
+        }, {
             label: 'Particles > 2.5um',
             fill: false,
             borderColor: "rgba(170,0,255,1.0)",
-        },{
+        }, {
             label: 'Particles > 5.0um',
             fill: false,
             borderColor: "rgba(255,0,255,1.0)",
-        },{
+        }, {
             label: 'Particles > 10.0 um',
             fill: false,
             borderColor: "rgba(255,0,150,1.0)",
         }]
     },
     options: {
-        legend: { display: false }/*,
-        scales: {
+        legend: { display: false },
+        /*scales: {
             yAxes: [{ ticks: { min: 6, max: 16 } }],
-        }*/
+        },*/
+        plugins: {
+            zoom: {
+                zoom: {
+                    wheel: {
+                        enabled: true,
+                    },
+                    pinch: {
+                        enabled: true
+                    },
+                    mode: 'xy',
+                }
+            }
+        }
     }
 });
 
@@ -77,7 +90,7 @@ function addData(chart, label, data) {
     /*chart.data.datasets.forEach((dataset) => {
         dataset.data.push(data);
     });*/
-    for (let i=0; i < 12; i++) {
+    for (let i = 0; i < 12; i++) {
         chart.data.datasets[i].data.push(data[i])
     }
     chart.update();
