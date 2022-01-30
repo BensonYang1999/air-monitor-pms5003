08b2c13b7a7e333c572c3ef57a7419927dea5212
BensonYang <benson2219@gmail.com>
2022-01-30 16:22:07 +0800

    2022/1/30 test 2

---
+++ website/public/index.js
@@ -0,0 +1,68 @@
+const socket = io.connect(window.location.origin);
+
+var xValues = []
+var data_count = 0;
+
+var history = new Chart("myChart", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            fill: false,
+            borderColor: "rgba(255,0,0,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(255,140,0,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(255,255,0,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(140,255,0,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(0,255,0,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(0,255,140,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(0,255,255,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(0,0,255,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(110,0,255,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(170,0,255,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(255,0,255,1.0)",
+        },{
+            fill: false,
+            borderColor: "rgba(255,0,150,1.0)",
+        }]
+    },
+    options: {
+        legend: { display: false }/*,
+        scales: {
+            yAxes: [{ ticks: { min: 6, max: 16 } }],
+        }*/
+    }
+});
+
+socket.on("new_data", data => {
+    //data_count += 1;
+    //addData(history, data_count, data)
+    history.addData(data, ++data_count)
+});
+
+/*function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}*/
--- website/serv.js
+++ website/serv.js
@@ -9,7 +9,7 @@ var data = [];
 app.use(express.static(__dirname + "/public"));
 
 app.get("/recv_data", (req, res) => {
-    var new_data = {
+    /*var new_data = {
         pm10_standard: parseInt(req.query.pm10_standard),
         pm25_standard: parseInt(req.query.pm25_standard),
         pm100_standard: parseInt(req.query.pm100_standard),
@@ -22,7 +22,21 @@ app.get("/recv_data", (req, res) => {
         particles_25um: parseInt(req.query.particles_25um),
         particles_50um: parseInt(req.query.particles_50um),
         particles_100um: parseInt(req.query.particles_100um)
-    }
+    }*/
+    var new_data = [
+        parseInt(req.query.pm10_standard),
+        parseInt(req.query.pm25_standard),
+        parseInt(req.query.pm100_standard),
+        parseInt(req.query.pm10_env),
+        parseInt(req.query.pm25_env),
+        parseInt(req.query.pm100_env),
+        parseInt(req.query.particles_03um),
+        parseInt(req.query.particles_05um),
+        parseInt(req.query.particles_10um),
+        parseInt(req.query.particles_25um),
+        parseInt(req.query.particles_50um),
+        parseInt(req.query.particles_100um)
+    ]
 
     console.log(new_data);
     data.push(new_data);
